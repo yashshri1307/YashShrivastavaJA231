338676abffc2480571201a00d105bc1b9eabdf06
yash shrivastava <yashshrivastava25@gmail.com>
2024-11-22 18:53:23 +0530

    initial

--- CricketTeamManagementSystem/src/main/java/com/hexaware/CricketTeamManagementSystem/exception/InvalidRoleException.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.CricketTeamManagementSystem.exception;
-
-public class InvalidRoleException extends RuntimeException{
-
-	public InvalidRoleException(String message) {
-		super(message);
-	}
-   
-}
